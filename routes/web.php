468e09dc22a851a606b252042dbeaa6d72a3ea39
MDB-sys <markusdaniel34@gmail.com>
2025-04-09 15:21:04 +0700

    RPL25-1 <membuat model dan controller mahasiswa>

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 
-use App\Http\Controllers\ControllerRuangan;
+use App\Http\Controllers\ControllerMahasiswa;
 
+use App\Http\Controllers\ControllerRuangan;
 use Illuminate\Support\Facades\Route;
+
 
 
 Route::get('/', function () {
@@ -18,4 +20,14 @@ Route::controller(ControllerRuangan::class)->group(function () {
     Route::get('/ruangan/{id}/edit', 'edit')->name('ruangan.edit');
     Route::put('/ruangan/{id}', 'update')->name('ruangan.update');
     Route::delete('/ruangan/{id}', 'destroy')->name('ruangan.destroy');
+});
+
+
+Route::controller(ControllerMahasiswa::class)->group(function () {
+    Route::get('/mahasiswa', 'index')->name('mahasiswa.index');
+    Route::get('/mahasiswa/create', 'create')->name('mahasiswa.create');
+    Route::post('/mahasiswa/store', 'store')->name('mahasiswa.store');
+    Route::get('/mahasiswa/{id}/edit', 'edit')->name('mahasiswa.edit');
+    Route::put('/mahasiswa/{id}', 'update')->name('mahasiswa.update');
+    Route::delete('/mahasiswa/{id}', 'destroy')->name('mahasiswa.destroy');
 });
