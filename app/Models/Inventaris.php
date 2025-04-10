8882539b68d6b0ec99704f86ca7579c1e70eefae
M.Fadli Syauqi ali <talingo200@gmail.com>
2025-04-10 08:03:05 +0700

    Membuat app layout, lalu membuat view untuk inventaris admin dan mahasiswa, mengubah route inventaris bagi mahasiswa dan admin
    Membuat app layout, lalu membuat view untuk inventaris admin dan mahasiswa, mengubah route inventaris bagi mahasiswa dan admin

--- app/Models/Inventaris.php
+++ app/Models/Inventaris.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Inventaris extends Model
 {
-    
+    use HasFactory;
+
+    protected $table = 'inventaris'; // pastikan tabel
+    protected $primaryKey = 'id_inventaris'; // inilah solusinya
+
+    // Jika kamu tidak pakai kolom 'id' default
+    public $incrementing = true;
+    protected $keyType = 'int';
 }
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route buat inventaris umum 
 
-Route::get('/inventaris', [InventarisController::class, 'index'])->name('inventaris.index');
-Route::get('/inventaris/{id}', [InventarisController::class, 'show'])->name('inventaris.show');
+// Admin Inventaris CRUD
+Route::get('/admin/inventaris', [InventarisController::class, 'index'])->name('inventaris.index');
+Route::prefix('admin')->group(function () {
+    Route::post('/inventaris', [InventarisController::class, 'store'])->name('inventaris.store');
+});
+Route::get('/admin/inventaris/create', [InventarisController::class, 'create'])->name('inventaris.create');
+Route::get('/admin/inventaris/{id}/edit', [InventarisController::class, 'edit'])->name('inventaris.edit');
+Route::delete('/admin/inventaris/{id}', [InventarisController::class, 'destroy'])->name('inventaris.destroy');
+
+
+// Mahasiswa Inventaris View
+Route::prefix('mahasiswa')->group(function () {
+    Route::get('/inventaris', [InventarisController::class, 'mahasiswaIndex'])->name('mahasiswa.inventaris.index');
+    Route::get('/inventaris/{id}', [InventarisController::class, 'mahasiswaShow'])->name('mahasiswa.inventaris.show');
+});
