b91897d2ba124015e4412c4027d0d67167cdf95d
ailsyafredericaaldora <ailsyafredericaaldora@gmail.com>
2025-04-16 12:48:54 +0700

    Update Mahasiswa.php

--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    //
+    //protected $table = 'mahasiswa';
+protected $primaryKey = 'id';
+protected $fillable = [
+    'name',
+    'email',
+    'phone',
+    'address'
+];
 }
