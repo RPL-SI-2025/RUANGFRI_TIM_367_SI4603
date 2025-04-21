5993efa6dd049f2fc48a6fbc7cfefaca9b16f52e
ailsyafredericaaldora <ailsyafredericaaldora@gmail.com>
2025-04-21 13:03:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::prefix('mahasiswa')->group(function () {
     Route::get('/inventaris/{id}', [InventarisController::class, 'mahasiswaShow'])->name('mahasiswa.inventaris.show');
 });
 
-=======
+
 
 // Ruangan routes
 Route::controller(ControllerRuangan::class)->group(function () {
