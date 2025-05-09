736ca5561862ec16ca05194674ac23f417bad48a
MDB-sys <markusdaniel34@gmail.com>
2025-05-09 09:57:14 +0700

    update main

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ControllerMahasiswa;
 use App\Http\Controllers\RuanganController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\laporinventarisController;
-use App\Models\laporinventaris; 
+use App\Models\laporinventaris;
 use App\Http\Controllers\MahasiswaAuthController;
 use App\Http\Controllers\AdminLogistikController;
 use App\Http\Controllers\RuanganCartController;
@@ -18,7 +18,7 @@ use App\Http\Middleware\MahasiswaAuth;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Support\Facades\Auth;
- 
+
 
 
 
@@ -70,7 +70,7 @@ Route::prefix('admin')->group(function () {
     Route::put('/laporinventaris/{lapor_inventaris}', [laporinventarisController::class, 'update'])->name('admin.lapor_inventaris.update');
     Route::delete('/laporinventaris/{lapor_inventaris}', [laporinventarisController::class, 'destroy'])->name('admin.lapor_inventaris.destroy');
     Route::get('/laporinventaris/{lapor_inventaris}', [laporinventarisController::class, 'show'])->name('admin.lapor_inventaris.show');
-    
+
 
     Route::get('/lapor-ruangan', [PelaporanController::class, 'index'])->name('admin.lapor_ruangan.index');
     Route::get('/lapor-ruangan/{id}', [PelaporanController::class, 'show'])->name('admin.lapor_ruangan.show');
@@ -83,7 +83,7 @@ Route::prefix('admin')->group(function () {
     Route::put('/ruangan/{id}', [RuanganController::class, 'update'])->name('admin.katalog_ruangan.update');
     Route::delete('/ruangan/{id}', [RuanganController::class, 'destroy'])->name('admin.katalog_ruangan.destroy');
 
- 
+
 });
 
 
@@ -131,7 +131,7 @@ Route::middleware([\App\Http\Middleware\MahasiswaAuth::class])->prefix('mahasisw
         return view('mahasiswa.page.dashboard');
     })->name('mahasiswa.dashboard');
 
-    // Mahasiswa Inventaris View 
+    // Mahasiswa Inventaris View
     Route::prefix('mahasiswa')->group(function () {
         Route::get('/inventaris', [InventarisController::class, 'mahasiswaIndex'])->name('mahasiswa.katalog.inventaris.index');
         Route::get('/inventaris/{id}', [InventarisController::class, 'mahasiswaShow'])->name('mahasiswa.katalog.inventaris.show');
@@ -139,7 +139,7 @@ Route::middleware([\App\Http\Middleware\MahasiswaAuth::class])->prefix('mahasisw
         Route::get('/ruangan', [RuanganController::class, 'mahasiswaIndex'])->name('mahasiswa.katalog.ruangan.index');
         Route::get('/ruangan/{id}', [RuanganController::class, 'mahasiswaShow'])->name('mahasiswa.katalog.ruangan.show');
     });
-    
+
     // Cart routes
     Route::get('/cart', [CartController::class, 'index'])->name('mahasiswa.cart.keranjang_inventaris.index');
     Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
